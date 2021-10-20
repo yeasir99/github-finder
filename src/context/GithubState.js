256b06fe0a582b26c4101f0aa289582e95821638
Yeasir Ahamed <yahamed007@gmail.com>
2021-10-20 20:05:27 +0600

    update github context

--- src/context/GithubState.js
+++ src/context/GithubState.js
@@ -1,83 +1,26 @@
-import React, {useReducer} from 'react'
-import axios from 'axios'
-import githubContext from './githubContext'
-import githubReducer from './githubReducer'
-import * as ACTION from './Types'
+import React, {useReducer, createContext, useContext} from 'react'
 
-const GithubState = ({children}) => {
-  const initialState = {
+import githubReducer from './githubReducer'
+
+const githubContext = createContext()
+
+export const useGithubState = () => {
+  const context = useContext(githubContext)
+  if (!context) {
+    throw new Error(
+      'Component must be wrapped with in GithubStateContextProvider',
+    )
+  }
+  return context
+}
+
+export const GithubState = props => {
+  const [state, dispatch] = useReducer(githubReducer, {
     allUser: [],
     singleUser: {},
     repos: [],
     loading: false,
-  }
+  })
 
-  const [state, dispatch] = useReducer(githubReducer, initialState)
-
-  //Search user
-
-  const searchUser = async text => {
-    setLoading()
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`,
-    )
-    dispatch({
-      type: ACTION.SEARCH_USERS,
-      payload: res.data.items,
-    })
-  }
-
-  // Get User
-  const getUser = async username => {
-    setLoading()
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`,
-    )
-
-    dispatch({
-      type: ACTION.GET_USER,
-      payload: res.data,
-    })
-  }
-
-  // Get Repos
-  const getUserRepos = async username => {
-    setLoading()
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`,
-    )
-
-    dispatch({
-      type: ACTION.GET_REPOS,
-      payload: res.data,
-    })
-  }
-
-  // Clear Users
-  const clearUsers = () => dispatch({type: ACTION.CLEAR_USERS})
-
-  // Set Loading
-  const setLoading = () => dispatch({type: ACTION.SET_LOADING})
-
-  return (
-    <githubContext.Provider
-      value={{
-        allUser: state.allUser,
-        singleUser: state.singleUser,
-        repos: state.repos,
-        loading: state.loading,
-        searchUser,
-        getUser,
-        getUserRepos,
-        clearUsers,
-      }}
-    >
-      {children}
-    </githubContext.Provider>
-  )
+  return <githubContext.Provider value={[state, dispatch]} {...props} />
 }
-
-export default GithubState
---
+++ src/context/githubActions.js
@@ -0,0 +1,49 @@
+//Search user
+import axios from 'axios'
+import * as ACTION from './Types'
+
+export const searchUser = async (text, dispatch) => {
+  setLoading()
+
+  const res = await axios.get(
+    `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`,
+  )
+  dispatch({
+    type: ACTION.SEARCH_USERS,
+    payload: res.data.items,
+  })
+}
+
+// Get User
+export const getUser = async (username, dispatch) => {
+  setLoading()
+
+  const res = await axios.get(
+    `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`,
+  )
+
+  dispatch({
+    type: ACTION.GET_USER,
+    payload: res.data,
+  })
+}
+
+// Get Repos
+export const getUserRepos = async (username, dispatch) => {
+  setLoading()
+
+  const res = await axios.get(
+    `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`,
+  )
+
+  dispatch({
+    type: ACTION.GET_REPOS,
+    payload: res.data,
+  })
+}
+
+// Clear Users
+export const clearUsers = dispatch => dispatch({type: ACTION.CLEAR_USERS})
+
+// Set Loading
+export const setLoading = dispatch => dispatch({type: ACTION.SET_LOADING})
